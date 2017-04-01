5f736030f4c27b6c1cc26eb2c5a592067b0eb49b
shashankpr <s.p.rao@student.tudelft.nl>
2017-04-01 17:44:45 +0200

    New code with every component in one page

--- chatbot.py
+++ chatbot.py
@@ -21,26 +21,86 @@ import os
 import requests
 from sys import argv
 from wit import Wit
-import messenger
-#from bottle import Bottle, request, debug
 from flask import Flask, request
 
 # Wit.ai parameters
 #WIT_TOKEN = os.environ.get('WIT_TOKEN')
-
 WIT_TOKEN = 'GLEQMX7YW4IR4TM5S5B2TUBPWJTCFDQQ'
 # Messenger API parameters
 #FB_PAGE_TOKEN = os.environ.get('FB_PAGE_TOKEN')
 FB_PAGE_TOKEN = 'EAACyYurnCYEBANLpIzLGAYA38fGeIXwS0v0q4LiZC7eH45LLnGMNe43ePXRYYDqWthXuW1qRJ5IZBxV3Ipek5ZCe3iMs0hZCZAN4BAOLZB41pHUTT0sZBVxEZAZCsiYckz1PDgAJVqzQMMhVAZCHzcKY49rtjwIO5lKeM8nUZBGTxGWCwZDZD'
+
 # A user secret to verify webhook get request.
-FB_VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
+#FB_VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
+
+FB_VERIFY_TOKEN = 'mischief_managed'
 
 # Setup Flask Server
-#debug(True)
-#app = Flask(__name__)
 
-messageToSend = "Hi There !! I am Jarvis and I'm awesome"
-done = False
+app = Flask(__name__)
+
+
+# Facebook Messenger GET Webhook
+@app.route('/', methods=['GET'])
+def messenger_webhook():
+    """
+    A webhook to return a challenge
+    """
+    verify_token = request.args.get('hub.verify_token')
+    # check whether the verify tokens match
+    if verify_token == FB_VERIFY_TOKEN:
+        # respond with the challenge to confirm
+        challenge = request.args.get('hub.challenge')
+        return challenge
+    else:
+        return 'Invalid Request or Verification Token'
+
+
+# Facebook Messenger POST Webhook
+@app.route('/', methods=['POST'])
+def messenger_post():
+    """
+    Handler for webhook (currently for postback and messages)
+    """
+    data = request.json
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            # get all the messages
+            messages = entry['messaging']
+            if messages[0]:
+                # Get the first message
+                message = messages[0]
+                # Yay! We got a new message!
+                # We retrieve the Facebook user ID of the sender
+                fb_id = message['sender']['id']
+                # We retrieve the message content
+                text = message['message']['text']
+                # Let's forward the message to the Wit.ai Bot Engine
+                # We handle the response in the function send()
+                client.run_actions(session_id=fb_id, message=text)
+    else:
+        # Returned another event
+        return 'Received Different Event'
+    return None
+
+
+def fb_message(sender_id, text):
+    """
+    Function for returning response to messenger
+    """
+    data = {
+        'recipient': {'id': sender_id},
+        'message': {'text': text}
+    }
+    # Setup the query string with your PAGE TOKEN
+    #qs = 'access_token=' + FB_PAGE_TOKEN
+    # Send POST request to messenger
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages",
+                         params={"access_token": FB_PAGE_TOKEN},
+                         json=data,
+                         headers={'Content-type': 'application/json'})
+    return resp.content
+
 
 def first_entity_value(entities, entity):
     """
@@ -53,11 +113,6 @@ def first_entity_value(entities, entity):
         return None
     return val['value'] if isinstance(val, dict) else val
 
-def say(sender_id, context, response):
-    global messageToSend
-    messageToSend = str(response)
-    global done
-    done = True
 
 def send(request, response):
     """
@@ -67,18 +122,13 @@ def send(request, response):
     fb_id = request['session_id']
     text = response['text']
     # send message
-    messenger.send_message(FB_PAGE_TOKEN, fb_id, text)
+    fb_message(fb_id, text)
 
-def merge(context, entities):
+
+def get_forecast(request):
+    context = request['context']
+    entities = request['entities']
     loc = first_entity_value(entities, 'location')
-    if loc:
-        context['loc'] = loc
-    return context
-
-
-def fetch_weather(context):
-    loc = context['loc']
-    #loc = first_entity_value(entities, 'location')
     if loc:
         # This is where we could use a weather service api to get the weather.
         context['forecast'] = 'sunny'
@@ -90,22 +140,15 @@ def fetch_weather(context):
             del context['forecast']
     return context
 
-
-    # Setup Actions
+# Setup Actions
 actions = {
-    'say'  : say,
-    'send' : send,
-    'merge': merge,
-    'fetch-weather': fetch_weather,
-    }
+    'send': send,
+    'fetch-weather': get_forecast,
+}
 
-    # Setup Wit Client
-def getWit():
-    client = Wit(access_token=WIT_TOKEN, actions=actions)
+# Setup Wit Client
+client = Wit(access_token=WIT_TOKEN, actions=actions)
 
-    return client
-
-
-# if __name__ == '__main__':
-#     # Run Server
-#     app.run(host='0.0.0.0', port=argv[1])
+if __name__ == '__main__':
+    # Run Server
+    app.run()
