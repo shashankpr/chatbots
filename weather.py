ec8acc174f0c6a54522adb82ce2edc392d9d9530
Shashank <shashank1692@gmail.com>
2017-04-05 06:33:27 +0530

    weather api call

--- weather.py
+++ weather.py
@@ -1,11 +1,20 @@
-import logging
+#import logging
+#
+#logging.basicConfig(level=logging.DEBUG)
+#def inWeather(location):
+#	forecast = 'rainy'
+#	logging.info("{}".format(main_forecast))
+#	return main_forecast
+#name = 'London'
+#inWeather(name)
+import pyowm
 
-logging.basicConfig(level=logging.DEBUG)
+owm = pyowm.OWM('929d3119163895b3abe69e0c5905f1bd')
+#forecast = owm.daily_forecast("London")
+
 def inWeather(location):
-	forecast = 'rainy'
-	main_forecast = location + forecast
-	logging.info("{}".format(main_forecast))
-	return main_forecast
-
-name = 'London'
-inWeather(name)
+	obs = owm.weather_at_place(location)
+	w = obs.get_weather()
+	detailed_weather = w.get_detailed_status()
+	temp = w.get_temperature(unit='celsius')
+	return detailed_weather
